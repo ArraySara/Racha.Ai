afb0f2dbea408909e3897e165323f7c25debd8a4
ArraySara <sarahlourenco54@gmail.com>
2024-10-21 20:57:19 -0300

    Fix: Funcionamento do login

--- src/scripts/Evento.js
+++ src/scripts/Evento.js
@@ -1,31 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const eventBody = document.getElementById('event-body');
+document.getElementById("desconectar").addEventListener("click", function () {
+  localStorage.removeItem("usuario");
 
-    // Array de eventos simulados
-    const events = [
-        {
-            establishment: '88 Bier',
-            address: 'R. Nunes Valente, 1355 - Aldeota',
-            date: '10/04/2024',
-        },
-        {
-            establishment: '88 Bier',
-            address: 'R. Nunes Valente, 1355 - Aldeota',
-            date: '19/04/2024',
-        },
-        {
-            establishment: 'Floresta Complexo',
-            address: 'Av. Santos Dumont, 1788 - Aldeota',
-            date: '30/06/2024',
-        }
-    ];
+  window.location.href = "../usuario/Login.html";
+});
 
-    // Função para renderizar a lista de eventos
-    function renderEvents() {
-        events.forEach(event => {
-            const row = document.createElement('tr');
-            
-            row.innerHTML = `
+document.addEventListener("DOMContentLoaded", () => {
+  const usuarioLogado = JSON.parse(localStorage.getItem("usuario"));
+  const usuarioComponente = document.getElementById("nome-usuario");
+  usuarioComponente.innerText = usuarioLogado?.nome;
+
+  const eventBody = document.getElementById("event-body");
+
+  const eventosLocal = JSON.parse(localStorage.getItem("eventos")) || [];
+
+  // Função para renderizar a lista de eventos
+  function renderEvents() {
+    eventosLocal.forEach((event) => {
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
                 <td>${event.establishment}</td>
                 <td>${event.address}</td>
                 <td>${event.date}</td>
@@ -34,10 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     <img src="../../assets/trash.png" alt="Excluir" class="action-icons delete-icon">
                 </td>
             `;
-            
-            eventBody.appendChild(row);
-        });
-    }
 
-    renderEvents();
+      eventBody.appendChild(row);
+    });
+  }
+
+  renderEvents();
 });
